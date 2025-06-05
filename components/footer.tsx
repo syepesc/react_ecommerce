024a9109d21236d4103614295d8c97301dc01999
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-06-05 16:44:28 -0500

    UI progress: add header and footer for root pages

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+import { APP_NAME } from "@/lib/constants";
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+    return <footer className="border-t">
+        <p className="p-5 flex-center">
+            {currentYear} {APP_NAME}. All rights Reserved
+        </p>
+    </footer>;
+}
+
+export default Footer;
