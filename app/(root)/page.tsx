e78cda76289f9a4013cc54383d6faddd636c359b
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-06-09 13:06:33 -0500

    UI progress: create product card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,8 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 const HomePage = () => {
-  return <> Ecommerce App</>;
+  console.log(sampleData);
+  return <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />;
 };
 
 export default HomePage;
