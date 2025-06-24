67d67454480d8b8169987ea589916ca887f5fe0b
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-06-24 14:43:15 -0500

    Fix sign-in error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -14,7 +14,8 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
       password: formData.get("password"),
     });
 
-    signIn("credentials", user);
+    await signIn("credentials", user);
+
     return { success: true, message: "Signed in successfully" };
   } catch (error) {
     if (isRedirectError(error)) {
