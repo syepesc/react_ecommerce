35ea7601dcab3f20966613dbf090ff14578dbe4c
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-06-25 17:52:29 -0500

    Implement add to cart button

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -2,9 +2,9 @@ import { notFound } from "next/navigation";
 import ProductPrice from "@/components/shared/product/product-price";
 import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.actions";
-import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import ProductImages from "@/components/shared/product/product-images";
+import AddToCart from "@/components/shared/product/add-to-cart";
 
 const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) => {
   const params = await props.params;
@@ -66,7 +66,16 @@ const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) 
                 </div>
                 {product.stock > 0 && (
                   <div className="flex-center mt-4">
-                    <Button className="w-full">Add to cart</Button>
+                    <AddToCart
+                      item={{
+                        productId: product.id,
+                        name: product.name,
+                        slug: product.slug,
+                        price: product.price,
+                        quantity: 1,
+                        image: product.images![0],
+                      }}
+                    />
                   </div>
                 )}
               </CardContent>
---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { CartItem } from "@/types";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+import { toast } from "sonner";
+import { addItemToCart } from "@/lib/actions/cart.actions";
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item);
+
+    if (!res.success) {
+      toast.error(res.message);
+      return;
+    }
+
+    toast.success(res.message, {
+      action: {
+        label: "Go to Cart",
+        onClick: () => router.push("/cart"),
+      },
+    });
+  };
+
+  return (
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      <Plus />
+      Add to Cart
+    </Button>
+  );
+};
+
+export default AddToCart;
