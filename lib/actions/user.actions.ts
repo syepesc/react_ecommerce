987ec09cda357a66fc017034fe3ada592a277083
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-07-07 14:53:10 -0400

    Progress in cart checkout: payment method form

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,13 @@
 "use server";
 
-import { shippingAddressSchema, signInFormSchema, signUpFormSchema } from "@/lib/validators";
+import { paymentMethodSchema, shippingAddressSchema, signInFormSchema, signUpFormSchema } from "@/lib/validators";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatFormError } from "../utils";
 import { ShippingAddress } from "@/types";
+import { z } from "zod";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -101,6 +102,31 @@ export async function updateUserAddress(data: ShippingAddress) {
     });
 
     return { success: true, message: "User updated successfully" };
+  } catch (error) {
+    return { success: false, message: formatFormError(error) };
+  }
+}
+
+// Update user's payment method
+export async function updateUserPaymentMethod(data: z.infer<typeof paymentMethodSchema>) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+    if (!currentUser) throw new Error("User not found");
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
   } catch (error) {
     return { success: false, message: formatFormError(error) };
   }
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,3 +3,7 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Ecommerce App";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Modern ecommerce app created using nextjs";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+  ? process.env.PAYMENT_METHODS.split(", ")
+  : ["PayPal", "Stripe"];
+export const DEFAULT_PAYMENT_METHOD = process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
