a42f7051402bd22f105d05f5b659451b98d771b2
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-07-21 19:35:15 -0400

    Add tests for paypal generate token

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -3,7 +3,7 @@ const baseUrl = process.env.PAYPAL_API_URL || "https://sandbox.paypal.com";
 export const paypal = {};
 
 // Generate paypal access token
-async function generateAccessToken() {
+export async function generateAccessToken() {
   const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
   const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString("base64");
 
