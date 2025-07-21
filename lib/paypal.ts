6bc822f868827516009048b7c10bb0d219715d8d
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-07-21 19:18:18 -0400

    Implementing Paypal: Generate access token

---
+++ lib/paypal.ts
@@ -0,0 +1,29 @@
+const baseUrl = process.env.PAYPAL_API_URL || "https://sandbox.paypal.com";
+
+export const paypal = {};
+
+// Generate paypal access token
+async function generateAccessToken() {
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString("base64");
+
+  const response = await fetch(`${baseUrl}/v1/oauth2/token`, {
+    method: "POST",
+    body: "grant_type=client_credentials",
+    headers: {
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
+
+async function handleResponse(response: Response) {
+  if (response.status === 200 || response.status === 201) {
+    return response.json();
+  }
+  const errorMessage = await response.text();
+  throw new Error(errorMessage);
+}
